d54ac6ade4944ea645a47d1eb5fb516f267c0efe
Martijn van Maurik <martijn@vmaurik.nl>
2015-02-27 14:24:04 +0100

    Doh ... left echo!

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ option=${1:---install}
 
 if [ ${option} = "--install" ]
 then
-    echo /usr/bin/vh --install
+    /usr/bin/vh --install
 elif [ ${option} = "--run" ]
 then
-    echo /usr/bin/vh_daemon /usr/bin/verlihub
+    /usr/bin/vh_daemon /usr/bin/verlihub
 fi
